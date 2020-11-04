c753f56422e4dc8ef3a9d9a8b23f8396d9c474ec
jamae10 <52740452+jamae10@users.noreply.github.com>
2020-11-04 21:08:19 +0800

    window only

---
+++ FCFS/Form1.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace FCFS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 603);
+            this.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1000, 650);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1000, 650);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "First Come First Serve";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
